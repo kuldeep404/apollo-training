95da670ecc7673fd2e07d4184d791b37783d9a6a
Kuldeep Kumar <kuldeep.kumar@lapr117.successivetech.com>
2019-08-21 13:21:47 +0530

    changed files

--- src/server.js
+++ src/server.js
@@ -1,24 +1,19 @@
 import { ApolloServer, gql } from 'apollo-server-express';
-import * as express from 'express';
+import express from 'express';
 import { configuration } from './config';
-
 const app = express();
-export default class Server {
-  constructor() {
-    const typeDefs = gql`
-    type Query {
-      hello: String
-    }
-    `;
-    const resolvers = {
-      Query: {
-        hello: () => 'world',
-      },
-    };
-    const server = new ApolloServer({ typeDefs, resolvers });
-    server.applyMiddleware({ app });
-    app.listen({ port: configuration.port }, () => {
-      console.log('server is ready');
-    });
+const typeDefs = gql`
+  type Query {
+    hello: String
   }
-}
+`;
+const resolvers = {
+  Query: {
+    hello: () => 'world',
+  }
+};
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app });
+app.listen({ port: configuration.port }, () => {
+  console.log('server is ready');
+});
