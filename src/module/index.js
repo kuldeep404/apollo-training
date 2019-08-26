68412f715a7a9578acb3515312a92ad715d769ce
Nikita <46665675+nikitaagg93@users.noreply.github.com>
2019-08-26 16:53:22 +0530

    Update index.js
    resolved merge conflicts

--- src/module/index.js
+++ src/module/index.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { user as me } from './user';
 import { Trainee as trainee } from './trainee';
 // console.log(trainee);
@@ -8,15 +8,4 @@ const resolvers = {
     ...me,
   },
 };
-=======
-import  {user as me}  from './user';
-import {Trainee as trainee} from './trainee';
-// console.log(trainee);
-const resolvers = {
-    Query:{
-        trainee,
-        me
-    }
-}
->>>>>>> 99827ee9c2c53e15120c82262a420cb320df2bdc
 export default resolvers;
